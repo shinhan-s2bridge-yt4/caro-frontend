3d8b032492af3c2972e79752154f02c240590692
윤혜진 <yhjyhw1004@naver.com>
2026-02-11 12:09:38 +0900

    [FE] edit: app 폰트 적용 오류 수정

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,30 +1,10 @@
-import { useEffect } from 'react';
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
 import StorybookUIRoot from '../storybook';
 import { Slot } from 'expo-router';
-
-// 폰트 로딩 전까지 스플래시 화면 유지
-SplashScreen.preventAutoHideAsync();
 
 const isStorybookEnabled =
   process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true';
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    Pretendard: require('@/assets/fonts/PretendardVariable.ttf'),
-  });
-
-  useEffect(() => {
-    if (fontsLoaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded]);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
   if (isStorybookEnabled) {
     return <StorybookUIRoot />;
   }
